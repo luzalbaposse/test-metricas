19ac2597bbe2905c337947549bd4b8f06f4463d1
luzalbaposse <malbaposse@mail.utdt.edu>
2024-10-19 16:03:45 -0300

    feat: Ejercicio 2 al 5

--- analisis.r
+++ analisis.r
@@ -20,14 +20,8 @@ hist(historical_data$play.delay,
 # Añadir una línea para la media
 abline(v = mean_historical, col = "red", lwd = 2, lty = 2)
 
-
-
-# Cargar datos nuevos (grupo de prueba)
 new_data <- read.csv("datos_nuevos.csv")
 
-# Calcular la media de los nuevos datos (estimación de µ)
 mean_new <- mean(new_data$play.delay)
 
-# Mostrar la media estimada
 mean_new
-
